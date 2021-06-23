76258b71626d6944021749be9d958849f8bf24d4
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-06-23 15:41:04 +0900

    53. Handling POST Requests

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+// express.json() 이 middleware이다
+// request와 response 사이에 stand 해서 middleware
+
 // // root URL '/'
 // // get : http method
 // app.get('/', (req, res) => {
@@ -39,6 +43,33 @@ app.get('/api/v1/tours', (req,res) => {
             tours //원래 tours:tours 인데 이름 같아서 생략
         }
     });
+});
+
+// URL은 GET과 같다
+// http method 만 get 에서 post 로 변경
+app.post('/api/v1/tours', (req,res) => {
+    // console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    // 지금 tours는 tour object 들의 array임
+    // tours.length-1 로 마지막 tour object의 정보를 불러오고
+    // .id 로 해당 tour의 id 속성값을 가져옴
+    // + 1 을 해줌으로써 newId 생성
+
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour); //tours array에 newTour을 넣어준다
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+        // 200 -> OK
+        // 201 -> created
+    });
+    // res.send('Done');
 });
 
 
