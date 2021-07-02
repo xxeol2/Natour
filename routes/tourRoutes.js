5b3b1e617bef9db72de28c828fb62065e57830e5
xxeol2 <71129059+xxeol2@users.noreply.github.com>
2021-07-02 23:33:32 +0900

    65. Chaining Multiple Middleware Functions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,10 +7,16 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
+
 router
     .route('/') // api/v1/tours 를 뜻한다
     .get(tourController.getAllTours)
-    .post(tourController.createTour);
+    .post(tourController.checkBody, tourController.createTour);
 
 router
     .route('/:id') // api/v1/tours/:id 를 뜻한다
